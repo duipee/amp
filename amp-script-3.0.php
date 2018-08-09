7370313061ad7b44e5c31a34f06b875d07a3287c
duipee <42163278+duipee@users.noreply.github.com>
2018-08-09 18:15:11 +0700

    Update amp-script-3.0.php

--- amp-script-3.0.php
+++ amp-script-3.0.php
@@ -233,5 +233,34 @@
 	  $('[data-toggle="tooltip"]').tooltip()
 	})	
 	</script>
+	  
+	  <script src="https://technicalseo.com/resources/tools/iframeResizer.min.js"></script>
+<script type="text/javascript">
+	iFrameResize({
+		log                     : true,                  // Enable console logging
+		enablePublicMethods     : true,                  // Enable methods within iframe hosted page
+		resizedCallback         : function(messageData){ // Callback fn when resize is received
+			$('p#callback').html(
+				'<b>Frame ID:</b> '    + messageData.iframe.id +
+				' <b>Height:</b> '     + messageData.height +
+				' <b>Width:</b> '      + messageData.width + 
+				' <b>Event type:</b> ' + messageData.type
+			);
+		},
+		messageCallback         : function(messageData){ // Callback fn when message is received
+			$('p#callback').html(
+				'<b>Frame ID:</b> '    + messageData.iframe.id +
+				' <b>Message:</b> '    + messageData.message
+			);
+			alert(messageData.message);
+		},
+		closedCallback         : function(id){ // Callback fn when iFrame is closed
+			$('p#callback').html(
+				'<b>IFrame (</b>'    + id +
+				'<b>) removed from page.</b>'
+			);
+		}
+	});
+</script> 
   </body>
 </html>
